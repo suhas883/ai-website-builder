c61a539bab97532fbd60713664692c52711ce8e3
suhas883 <suhas883@gmail.com>
2025-12-14 23:02:06 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  experimental: {
+    esmExternals: true,
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
